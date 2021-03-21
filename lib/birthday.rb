666bffcdeeb2e450e0b98b8e9f524d8bb328b71c
Harrison Price <hprice518@gmail.com>
2021-03-21 17:53:21 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,11 +4,11 @@
 #   "Amanda" => 27
 # }
 
+
+
+
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy birthday #{kids_name}! You are now #{age} years old!"
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
-  
 end
-
-
